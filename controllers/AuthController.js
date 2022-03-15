f863f2ba6f7acceb95b801f7d5a494c0b860dba0
andrey-rsn <korowai98.ag@gmail.com>
2022-03-15 19:15:54 +0300

    implemented_docker

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,13 +5,8 @@ exports.login = function (request, response){
     var password=request.body.password;
     var isCrypto=request.body.isCrypto;
     var authManager = new Auth(login,password,isCrypto);
-<<<<<<< HEAD
     var res1=authManager.auth();
     if(res1==200)
-=======
-    var res2=authManager.auth();
-    if(res2==200)
->>>>>>> branch_2
     {
         
         response.render("nextPage.hbs");
